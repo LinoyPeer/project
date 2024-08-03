c07a1ff0b3c7c4f25b73ad0f070c7684f33ea0b4
LinoyPeerProject <linoypeer1@gmail.com>
2024-08-03 19:35:38 +0300

    modified:   ProjectJs/Projects/snakeGameProject/sankeGameMobileProject/game.css modified:   ProjectJs/Projects/snakeGameProject/sankeGameMobileProject/main.js

--- ProjectJs/Projects/snakeGameProject/sankeGameMobileProject/main.js
+++ ProjectJs/Projects/snakeGameProject/sankeGameMobileProject/main.js
@@ -1,16 +1,14 @@
 import { Game } from "./game.js";
 
 const canvas = document.getElementById("app");
-let directionsDiv = document.querySelector('.containerForArrowDirections');
-canvas.style.opacity = '0.4'
-directionsDiv.style.opacity = '0.4'
+const directionsDiv = document.querySelector('.containerForArrowDirections');
+const countdown = document.getElementById('countdown');
+const counter = document.createElement('div');
 
+canvas.style.opacity = '0.4';
+directionsDiv.style.opacity = '0.4';
 
 let counterValue = 3;
-
-const countdown = document.getElementById('countdown');
-
-const counter = document.createElement('div');
 counter.style.fontSize = '40vw';
 counter.innerText = counterValue;
 countdown.appendChild(counter);
@@ -23,46 +21,41 @@ const timer = setInterval(() => {
         clearInterval(timer);
         counter.innerText = 'GO!';
         setTimeout(() => {
-            countdown.remove();
+            countdown.style.display = 'none';
+            directionsDiv.style.opacity = '1';
+            canvas.style.opacity = '1';
+            startGame();
         }, 1000);
     }
 }, 1000);
-setTimeout(() => {
 
-    const canvas = document.getElementById("app");
+function startGame() {
     canvas.width = 300;
     canvas.height = 300;
-    canvas.style.opacity = '1'
-    directionsDiv.style.opacity = '1'
-
+    canvas.style.opacity = '1';
 
     const game = new Game(canvas, 20);
-
     game.start();
 
-    let up = document.querySelector('.up')
-    let down = document.querySelector('.down')
-    let left = document.querySelector('.left')
-    let right = document.querySelector('.right')
+    let up = document.querySelector('.up');
+    let down = document.querySelector('.down');
+    let left = document.querySelector('.left');
+    let right = document.querySelector('.right');
 
     up.addEventListener('click', () => {
         console.log("up");
         game.handleInput("ArrowUp");
-    })
+    });
     down.addEventListener('click', () => {
         console.log("down");
         game.handleInput("ArrowDown");
-
-    })
+    });
     left.addEventListener('click', () => {
         console.log("left");
         game.handleInput("ArrowLeft");
-
-    })
+    });
     right.addEventListener('click', () => {
         console.log("right");
         game.handleInput("ArrowRight");
-
-    })
-
-}, 4000);
+    });
+}
